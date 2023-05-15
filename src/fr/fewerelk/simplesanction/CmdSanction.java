7cf81903630657cc4e4913ab7629511d2238df9e
FewerElk <gabriel@trouilhet.fr>
2023-05-15 18:51:18 +0200

    One bug to fix

--- src/fr/fewerelk/simplesanction/CmdSanction.java
+++ src/fr/fewerelk/simplesanction/CmdSanction.java
@@ -4,26 +4,46 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
 import org.bukkit.BanEntry;
 import org.bukkit.BanList;
-import java.util.UUID;
 import org.bukkit.ChatColor;
 import org.bukkit.BanList.Type;
+
+import java.util.Date;
+import java.util.UUID;
 
 public class CmdSanction implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        //if(sender instanceof Player){
-    	if(true){
-            Player p = (Player) sender;
-            if (p.isBanned()) {
-            	
-            } else {
-            	p.kickPlayer("Banned for a good reason");
-            	UUID playerId = p.getUniqueId();
-            	BanList bl;
-            	bl.getBanEntries(Type.NAME).addBan(playerId, "Banned because you are banned", null, p);
+    	if (cmd.getName().equalsIgnoreCase("sanction")) {
+            //get sender
+            Player source = (Player) sender;
+
+            //does the sender allowed to use the command ?
+            if(source.hasPermission("simplesanction.sanction")) {
+                //good number of args ?
+                if(args.length == 2) {
+                    //get the target
+                    Player target = Bukkit.getPlayer(args[0]);
+
+                    //create the Date object that define when the ban expires (here 7 days)
+                    int bantime = 604800000;
+                    Date date_now = new Date();
+                    Date expires = new Date(date_now.getTime() + bantime);
+
+                    
+                    //get the reason
+                    String reason = ChatColor.DARK_RED + "You are banned by" + source.getDisplayName() + ". Reason : " + args[1] + ".Expires on : " + expires;
+
+                    //add the ban to the blacklist (or banlist)
+                    Bukkit.getBanList(Type.IP).addBan(Bukkit.MinecraftServer.getServer().getUserCache().getProfile(target), reason, expires, source);
+                    Bukkit.getServer().broadcastMessage(ChatColor.DARK_RED + "The player " + target.getDisplayName() + " has been banned !" + ChatColor.YELLOW + " Be carefull !");
+                } else {
+                    source.sendMessage(ChatColor.DARK_RED + "[SimpleSanction] : Failed : bad number of arguments. Usage : /sanction <player> <reason>.");
+                    return true;
+                }
             }
         }
         return true;
