a65d1df1194760b8c77094802628cede93b80da6
FewerElk <gabriel@trouilhet.fr>
2023-05-16 13:39:38 +0200

    patching name error for onEnable() and onDisable()

--- src/fr/fewerelk/simplesanction/Main.java
+++ src/fr/fewerelk/simplesanction/Main.java
@@ -9,13 +9,13 @@ import org.bukkit.Bukkit;
 public class Main extends JavaPlugin {
 	
 	@Override
-	public void OnEnable() {
+	public void onEnable() {
 		this.getCommand("sanction").setExecutor(new CmdSanction());
 		Bukkit.getServer().broadcastMessage(ChatColor.GREEN + "SimpleSanction enabled without any issue !");
 	}
 	
 	@Override
-	public void OnDisable() {
+	public void onDisable() {
 		Bukkit.getServer().broadcastMessage(ChatColor.BLUE + "SimpleSanction disabled. Goodbye and see you soon !");
 	}
 	
